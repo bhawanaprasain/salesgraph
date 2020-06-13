9c7219d3da3b37cda76fcd17cbb22907dff2b5af
bhawana <bhawana.prs@gmail.com>
2020-06-13 10:10:34 +0545

    Home

---
+++ src/components/Customer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "../styles/customer.css"
+
+class Customer extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      // speed: 100,
+      autoplay: true
+    
+    };
+    return (
+      <div className="customer">
+        <h1>What our customers say</h1>
+        <Slider {...settings}>
+        <div className="item">
+          <p>It is best site . We can know what is in trend on market and shop without confusion</p>
+          <h2>Bhawana prasain</h2>
+          <h3>Customer</h3>
+        </div>
+        <div className="item">
+        <p>It is best site . We can know what is in trend on market and shop without confusion</p>
+          <h2>Bhawana prasain</h2>
+          <h3>Customer</h3>
+
+        </div>
+        <div className="item">
+        <p>It is best site . We can know what is in trend on market and shop without confusion</p>
+          <h2>Bhawana prasain</h2>
+          <h3>Customer</h3>
+
+        </div>
+      </Slider>
+
+      </div>
+     
+    );
+  }
+}
+
+export default Customer
